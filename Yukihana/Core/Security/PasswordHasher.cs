758ce765380090b25564dea0e026afbcad8637f1
zimavi <bar05022009@gmail.com>
2026-04-15 15:58:21 +0300

    add: Switch Sha256 with external lib
    Replace Sha256 implementation with library

--- Yukihana/Core/Security/PasswordHasher.cs
+++ Yukihana/Core/Security/PasswordHasher.cs
@@ -1,7 +1,8 @@
 // Yukihana OS 2026 Yukihana OS Contributors
 // Licensed under the Apache 2.0 License. See LICENSE for details.
 
-using Yukihana.Core.Security.Cryptography;
+using System.Text;
+using acryptohashnet;
 
 namespace Yukihana.Core.Security;
 
@@ -9,7 +10,12 @@ public static class PasswordHasher
 {
     public static string Hash(string password)
     {
-        return Sha256.ComputeHashString(password);
+        var sha = new Sha2_256();
+        return Convert.ToHexString(
+            sha.ComputeHash(
+                Encoding.UTF8.GetBytes(password)
+            )
+        );
     }
 
     public static bool Verify(string password, string hash) => Hash(password) == hash;
